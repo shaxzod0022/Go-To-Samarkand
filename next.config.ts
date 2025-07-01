9cdcf74ef5b9ba02a1546d2a130eac3006892953
shaxzod0022 <ilgeldiyevshaxzod@gmail.com>
2025-07-01 12:37:51 +0500

    next-intl

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
+import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
-
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
